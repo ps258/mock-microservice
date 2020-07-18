55d5dffcd9b52f4069ebd9020da24d40b299be68
Peter Stubbs <peter.stubbs@outlook.com>
2020-07-18 18:18:22 +0100

    basic microserver

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var fileName *string
+var port *string
+var contentType *string
+
+func serveFile (w http.ResponseWriter, req *http.Request) {
+	fmt.Println("[INFO]Opening " + *fileName)
+	fileContents, err := ioutil.ReadFile(*fileName)
+	if err != nil {
+		fmt.Println("[FATAL]Unable to load file "+*fileName+": ", err)
+		os.Exit(1)
+	}
+	w.Header().Set("Content-Type", *contentType)
+	fmt.Fprintf(w, string(fileContents))
+}
+
+func main () {
+	port = flag.String("port", "8080", "The port to listen on")
+	fileName = flag.String("file", "", "File to serve")
+	contentType = flag.String("contentType", "text/plain", "The content type to put into the Content-Type header")
+	flag.Parse()
+	http.HandleFunc("/", serveFile)
+	http.ListenAndServe("192.168.1.182:"+*port, nil)
+}
