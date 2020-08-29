ad04e4c624d4fb68910c58be9c95574d9e4788d4
Peter Stubbs <peter.stubbs@outlook.com>
2020-08-29 14:38:43 +0100

    read file on startup not for every call

--- mock-ms.go
+++ mock-ms.go
@@ -11,25 +11,27 @@ import (
 var fileName *string
 var port *string
 var contentType *string
+var fileContents []byte
 
 func serveFile (w http.ResponseWriter, req *http.Request) {
-	fmt.Println("[INFO]Serving " + *fileName)
-	fileContents, err := ioutil.ReadFile(*fileName)
-	if err != nil {
-		fmt.Println("[FATAL]Unable to load file "+*fileName+": ", err)
-		os.Exit(1)
-	}
+	//fmt.Println("[INFO]Serving " + *fileName)
 	w.Header().Set("Content-Type", *contentType)
 	fmt.Fprintf(w, string(fileContents))
 }
 
 func main () {
+  var err error
 	port = flag.String("port", "8080", "The port to listen on")
 	fileName = flag.String("file", "", "File to serve")
 	contentType = flag.String("contentType", "text/plain", "The content type to put into the Content-Type header")
 	flag.Parse()
+	fileContents, err = ioutil.ReadFile(*fileName)
+	if err != nil {
+		fmt.Println("[FATAL]Unable to load file "+*fileName+": ", err)
+		os.Exit(1)
+	}
 	http.HandleFunc("/", serveFile)
-	err := http.ListenAndServe(":"+*port, nil)
+	err = http.ListenAndServe(":"+*port, nil)
 	if err != nil {
 		fmt.Println("[FATAL]Unable to serve on port "+*port, err)
 	}
