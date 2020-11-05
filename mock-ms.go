f3c46ad6c5cb78e4a76bc933e853e45893608759
Peter Stubbs <peter.stubbs@outlook.com>
2020-11-05 17:42:58 +0000

    added option to return the time rather than a statit file

--- mock-ms.go
+++ mock-ms.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+  "time"
 	"flag"
 	"fmt"
   "log"
@@ -9,33 +10,51 @@ import (
 	"os"
 )
 
-var fileName *string
-var port *string
-var contentType *string
-var fileContents []byte
-var verbose *bool
+var (
+  fileName     *string
+  port         *string
+  contentType  *string
+  fileContents []byte
+  verbose      bool
+  returnTime   bool
+)
 
 func serveFile (w http.ResponseWriter, req *http.Request) {
-  if *verbose {
+  if verbose {
     log.Println("[INFO]Serving " + *fileName)
   }
 	w.Header().Set("Content-Type", *contentType)
 	fmt.Fprintf(w, string(fileContents))
 }
 
+func serveTime (w http.ResponseWriter, req *http.Request) {
+  if verbose {
+    log.Println("[INFO]Serving Time")
+  }
+	w.Header().Set("Content-Type", *contentType)
+  fmt.Fprintf(w, time.Now().Format(time.RFC850) + "\n")
+}
+
 func main () {
   var err error
-	port = flag.String("port", "8080", "The port to listen on")
-	fileName = flag.String("file", "file.json", "File to serve")
+
+	port        = flag.String("port", "8080", "The port to listen on")
+	fileName    = flag.String("file", "file.json", "File to serve")
 	contentType = flag.String("contentType", "text/plain", "The content type to put into the Content-Type header")
-  verbose = flag.Bool("verbose", false, "Verbose output")
+  flag.BoolVar(&verbose, "verbose", false, "Verbose output")
+  flag.BoolVar(&returnTime,"time", false, "Return the timestamp rather than the contents of a file")
+
 	flag.Parse()
-	fileContents, err = ioutil.ReadFile(*fileName)
-	if err != nil {
-		fmt.Println("[FATAL]Unable to load file "+*fileName+": ", err)
-		os.Exit(1)
-	}
-	http.HandleFunc("/", serveFile)
+  if returnTime {
+    http.HandleFunc("/", serveTime)
+  } else {
+	  fileContents, err = ioutil.ReadFile(*fileName)
+	  if err != nil {
+		  fmt.Println("[FATAL]Unable to load file "+*fileName+": ", err)
+		  os.Exit(1)
+	  }
+	  http.HandleFunc("/", serveFile)
+  }
 	err = http.ListenAndServe(":"+*port, nil)
 	if err != nil {
 		fmt.Println("[FATAL]Unable to serve on port "+*port, err)
